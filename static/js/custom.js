d54d34a0862e4133d413bdf000a1e826eae65799
Fang Xi Lin <flin@cita.utoronto.ca>
2023-09-27 15:35:18 -0400

    optimize webpage auto scroll

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,17 +1,12 @@
 let tagline = document.querySelector(".tagline");
 
-// Stay on the same scroll even if you change page
-// let topScroll = localStorage.getItem("window-scroll");
-// if (topScroll !== null) {
-  // window.scrollTo(0, parseInt(topScroll, 10) );
-  // document.documentElement.scrollTop = parseInt(topScroll, 10);
-// }
+// If this is the first time on the domain in the session, don't scroll, if not, scroll to tagline.
 
-// window.addEventListener("beforeunload", () => {
-  // localStorage.setItem("window-scroll", window.scrollY);
-  // localStorage.setItem("window-scroll", document.documentElement.scrollTop);
-// });
+let topScroll = localStorage.getItem("window-scroll");
+if (topScroll !== null) {
+  document.documentElement.scrollTop = parseInt(tagline.offsetTop, 10);
+}
 
-// Scroll to the page tagline
-document.documentElement.scrollTop = parseInt(tagline.offsetTop, 10);
-// tagline.scrollIntoView();
+window.addEventListener("beforeunload", () => {
+  localStorage.setItem("window-scroll", true);
+});
