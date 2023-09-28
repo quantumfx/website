bfaa29c809930e7a57e4032c2484f7ad340c9662
Fang Xi Lin <flin@cita.utoronto.ca>
2023-09-28 13:20:27 -0400

    further scrolling functions for iOS

--- static/js/custom.js
+++ static/js/custom.js
@@ -15,13 +15,29 @@ function iOS() {
   || (navigator.userAgent.includes("Mac") && "ontouchend" in document)
 };
 
+export const scrollElementIntoView = (element: HTMLElement, behavior?: 'smooth' | 'instant' | 'auto') => {
+
+  let scrollTop = window.pageYOffset || window.scrollY || element.scrollTop
+
+  // Furthermore, if you have for example a header outside the iframe 
+  // you need to factor in its dimensions when calculating the position to scroll to
+  const headerOutsideIframe = window.parent.document.getElementsByClassName('myHeader')[0].clientHeight
+
+  const finalOffset = element.getBoundingClientRect().top + scrollTop + headerOutsideIframe
+
+  window.parent.scrollTo({
+    top: finalOffset,
+    behavior: behavior || 'auto'
+  })
+}
+
 let tagline = document.querySelector(".tagline");
 
 // If this is the first time on the domain in the session, don't scroll, if not, scroll to tagline.
 let topScroll = localStorage.getItem("window-scroll");
 if (topScroll !== null) {
   if (iOS() == true) {
-    tagline.scrollIntoView();
+    scrollElementIntoView(tagline,'instant')
   }
   else {
     document.documentElement.scrollTop = parseInt(tagline.offsetTop, 10);
