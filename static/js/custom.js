52bf432232d774e56ea7f45ab83cc040bff2b778
Fang Xi Lin <flin@cita.utoronto.ca>
2023-09-28 13:43:35 -0400

    more linting fixes

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,43 +1,43 @@
 // iOS detector
 // use backup if iphone
-let platform = navigator?.userAgentData?.platform || navigator?.platform || 'iPhone';
+let platform = navigator?.userAgentData?.platform || navigator?.platform || "iPhone";
 
 function iOS() {
   return [
-    'iPad Simulator',
-    'iPhone Simulator',
-    'iPod Simulator',
-    'iPad',
-    'iPhone',
-    'iPod'
+    "iPad Simulator",
+    "iPhone Simulator",
+    "iPod Simulator",
+    "iPad",
+    "iPhone",
+    "iPod"
   ].includes(platform)
   // iPad on iOS 13 detection
-  || (navigator.userAgent.includes("Mac") && "ontouchend" in document)
+  || (navigator.userAgent.includes("Mac") && "ontouchend" in document);
 }
 
-export const scrollElementIntoView = (element: HTMLElement, behavior?: 'smooth' | 'instant' | 'auto') => {
+function scrollElementIntoView(element, behavior) {
 
-  let scrollTop = window.pageYOffset || window.scrollY || element.scrollTop
+  let scrollTop = window.pageYOffset || window.scrollY || element.scrollTop;
 
   // Furthermore, if you have for example a header outside the iframe 
   // you need to factor in its dimensions when calculating the position to scroll to
-  // const headerOutsideIframe = window.parent.document.getElementsByClassName('myHeader')[0].clientHeight
+  // const headerOutsideIframe = window.parent.document.getElementsByClassName("myHeader")[0].clientHeight
 
-  const finalOffset = element.getBoundingClientRect().top + scrollTop// + headerOutsideIframe
+  const finalOffset = element.getBoundingClientRect().top + scrollTop;// + headerOutsideIframe
 
   window.parent.scrollTo({
-    top: finalOffset,
-    behavior: behavior || 'auto'
-  })
+    behavior: behavior || "auto",
+    top: finalOffset
+  });
 }
 
 let tagline = document.querySelector(".tagline");
 
-// If this is the first time on the domain in the session, don't scroll, if not, scroll to tagline.
+// If this is the first time on the domain in the session, don"t scroll, if not, scroll to tagline.
 let topScroll = localStorage.getItem("window-scroll");
 if (topScroll !== null) {
-  if (iOS() == true) {
-    scrollElementIntoView(tagline,'instant');
+  if (iOS() === true) {
+    scrollElementIntoView(tagline,"instant");
   }
   else {
     document.documentElement.scrollTop = parseInt(tagline.offsetTop, 10);
